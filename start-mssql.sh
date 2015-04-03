fda254dd1a9fc2730cf341e4d441cece5a7b844e
Sam Halliday <sam.halliday@gmail.com>
2015-04-03 13:18:18 +0200

    use snapshot

--- start-mssql.sh
+++ start-mssql.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# even if this was uncompressed, docker needs to do copy-on-write
-bzip2 -cd VirtualBox/MSSQL/VirtualXP.vdi.bz2 > VirtualBox/MSSQL/VirtualXP.vdi
-
 #VBoxManage setproperty machinefolder /root/VirtualBox
 #VBoxManage registervm /root/VirtualBox/MSSQL/MSSQL.vbox 
+VBoxManage snapshot MSSQL restore "MSSQL Up"
 VBoxManage startvm --type headless MSSQL
 
 # jdbc:jtds:sqlserver://localhost:2000
