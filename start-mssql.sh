7a953578756c48fe86f4e154398862f28577cd6e
Sam Halliday <sam.halliday@gmail.com>
2015-04-03 18:32:22 +0100

    use OVFs instead of tarballs

--- start-mssql.sh
+++ start-mssql.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cd /root/VirtualBox
-tar xf MSSQL.tar.gz
-
-sed -i 's|/dev/shm|/root|' /root/VirtualBox/MSSQL/MSSQL.vbox
-
 VBoxManage setproperty machinefolder /root/VirtualBox
-VBoxManage registervm /root/VirtualBox/MSSQL/MSSQL.vbox 
-VBoxManage snapshot MSSQL restore "MSSQL Up"
+VBoxManage import /root/MSSQL.ovf
+VBoxManage setextradata MSSQL VBoxInternal/Devices/pcbios/0/Config/BiosRom /root/mssql/pcbios.bin
+
+#cd /root/VirtualBox
+#tar xf MSSQL.tar.gz
+#sed -i 's|/dev/shm|/root|' /root/VirtualBox/MSSQL/MSSQL.vbox
+#VBoxManage registervm /root/VirtualBox/MSSQL/MSSQL.vbox 
+#VBoxManage snapshot MSSQL restore "MSSQL Up"
+
 VBoxManage startvm --type headless MSSQL
 
 # jdbc:jtds:sqlserver://localhost:2000
