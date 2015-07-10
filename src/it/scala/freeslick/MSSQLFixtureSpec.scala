950f429aa648b475d4d7a8acdfcb27688bd72973
Sue Carter <squishback@gmail.com>
2015-07-10 20:14:53 +0100

    Migrate SQLServer driver to use Slick 3.0

--- src/it/scala/freeslick/MSSQLFixtureSpec.scala
+++ src/it/scala/freeslick/MSSQLFixtureSpec.scala
@@ -3,9 +3,9 @@ package freeslick
 import java.util.UUID
 import org.scalatest._
 import org.slf4j.LoggerFactory
-import scala.slick.driver.JdbcProfile
-import scala.slick.jdbc.{ StaticQuery => Q }
-import scala.slick.jdbc.StaticQuery.interpolation
+import slick.driver.JdbcProfile
+import slick.jdbc.{ StaticQuery => Q }
+import slick.jdbc.StaticQuery.interpolation
 
 object MSSQL extends Tag("MSSQL")
 
@@ -34,7 +34,7 @@ abstract class MSSQLFixtureSpec extends fixture.WordSpecLike with BeforeAndAfter
 
   val driver = "net.sourceforge.jtds.jdbc.Driver"
   val profile: JdbcProfile = MSSQLProfile
-  import profile.simple._
+  import profile.api._
 
   // bill made me do it! https://github.com/scalatest/scalatest/issues/504
   override def tags = (testNames.map(_ -> Set.empty[String]).toMap ++ super.tags).mapValues(_ + MSSQL.name)
@@ -42,20 +42,17 @@ abstract class MSSQLFixtureSpec extends fixture.WordSpecLike with BeforeAndAfter
   override def beforeAll(): Unit = {
     log.info(s"creating database $dbname on $base")
     val db = Database.forURL(base, user = user, password = password, driver = driver)
-    db.withSession { implicit s =>
-      // NOTE: string interpolation does not work in CREATE statements
-      Q.updateNA(s"CREATE DATABASE $dbname").execute
-    }
+    db.run(SimpleDBIO{ implicit s => s"CREATE DATABASE $dbname"})
     log.info(s"created database $dbname")
   }
 
   override def afterAll(): Unit = {
     log.info(s"dropping database $dbname on $base")
     val db = Database.forURL(base, user = user, password = password, driver = driver)
-    db.withSession { implicit s =>
+    db.run(SimpleDBIO{ implicit s =>
       // NOTE: string interpolation does not work in DROP statements
-      Q.updateNA(s"DROP DATABASE $dbname").execute
-    }
+      Q.updateNA(s"DROP DATABASE $dbname")
+    })
     log.info(s"dropped database $dbname")
   }
 
